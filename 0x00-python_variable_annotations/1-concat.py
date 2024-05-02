7b298c4e42cba0442ae70793b208d22d5e131f56
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2024-05-02 14:20:01 +0000

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module of a type-annotated function concat
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Takes 2 strings as arguments and returns a concatenated string
+    """
+    return str1 + str2
